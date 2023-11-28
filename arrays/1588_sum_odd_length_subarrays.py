795bf4789ccc733793777d0e2f4cc5e56926b11b
Mike Nardella <1310994+nardellajr@users.noreply.github.com>
2023-11-27 21:13:34 -0500

    start 1588 solution

---
+++ arrays/1588_sum_odd_length_subarrays.py
@@ -0,0 +1,18 @@
+# 1588 Sum of all Odd length Subarrays
+
+# return sum of all possible odd-length subarrays
+
+def sumOddlengthSubarrays(arr: list[int]) -> int:
+    arr_dict = {}
+    for n in arr:
+        print(n)
+
+    return 99
+
+
+if __name__ == '__main__':
+    arr = [1, 4, 2, 5, 3]
+    sumOddlengthSubarrays(arr)
+
+    
+
