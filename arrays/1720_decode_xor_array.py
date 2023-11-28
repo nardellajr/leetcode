2cc2f0651ede9338f1c2465dfc19fb53b909d20d
Mike Nardella <1310994+nardellajr@users.noreply.github.com>
2023-11-27 21:03:28 -0500

    add comment to the solution for 1720 and bitwise_xor

--- arrays/1720_decode_xor_array.py
+++ arrays/1720_decode_xor_array.py
@@ -8,30 +8,45 @@
 def decode(encoded: list[int], first: int) -> list[int]:
     decoded = [first]
     for i in range(len(encoded)):
-        decoded.append(encoded[i] ^ decoded[i])
+        decoded.append(decoded[i] ^ encoded[i])
     return decoded
 
-
+# Time O(n)
+# Space O(1)
 def decode2(encoded: list[int], first: int) -> list[int]:
+    # encoded is the key
     # Initialize the array with the first element
     arr = [first]
 
-    # Iterate through the encoded array
+    # Iterate through the encoded (key) array
     for num in encoded:
-        # The next element in arr is the XOR of the last element in arr and the current element in encoded
+        # arr[-1] gets the last element in arr, then we do XOR with each encoded (key) element
+        # first time in loop, arr[-1] is 1 and first item in encoded was 1, 1 ^ 1
+        # converted to binary 2**0 = 1, bit is set so 1 * 1 = 1
+        # convert the other 1 to binary 2**0 = 1, bit is set so 1 * 1 = 1
+        # 1 XOR 1 = 0, 0 is appended to arr
+        # Now arr[-1] is 0 and num 2
+        # 2**0 = 1, bit not set, 0 * 1 = 0, num 2 = 2**0 = 1, bit no set 0 * 1 = 0, 2**1 = 2, bit set 1 * 2 = 2
+        # aligned by their rightmost bit, pad with leading zeroes, binary representation 00 ^ 10
+        # First bit 0 ^ 0 = 0
+        # Second bit 0 ^ 1 = 1
+        # result is binary 10, is 2 in decimal, which is appended to arr
         arr.append(arr[-1] ^ num)
 
-        return arr
+    return arr
 
 
 if __name__ == '__main__':
     encoded_example = [1, 2, 3]
     first_example = 1
 
-    print(decode(encoded_example, first_example))
-    # print(decode([6, 2, 7, 3], 4))
+    print(f"decode: {decode(encoded_example, first_example)}")
+    print(f"decode2: {decode2(encoded_example, first_example)}")
 
-    decoded_array = decode2(encoded_example, first_example)
-    print(decoded_array)
+    encoded_example = [6, 2, 7, 3]
+    first_example = 4
+    print(f"decode: {decode(encoded_example, first_example)}")
+    print(f"decode2: {decode2(encoded_example, first_example)}")
+
 
 
--- mikes_pratice/bitwise_xor.py
+++ mikes_pratice/bitwise_xor.py
@@ -3,41 +3,75 @@
 # 2**0, 2**1, 2**2, 2**3, 2**4
 #   1 ,   2,   4,     8,    16
 
-a = 5
-b = 3
 
-# Performing bitwise XOR
-# numbers are converted to their binary representations
-# 5 = 101 in binary
-# 3 = 011 in binary
-# Next the XOR operation is performed on each pair of bits (XOR yields 1 if bits are different, 0 if same)
-result = a ^ b
-# Starting from the RIGHT
-# First bit   1 ^ 1 = 0
-# Second bit  0 ^ 1 = 1
-# Third bit   1 ^ 0 = 1
-# resulting bits are compiled into binary 110, 6 in decimal
-# The rightmost bit represents 2**0 (which equals 1), when the bit is set
-# The second bit from the right represents 2**1 (which equals 2), when set
-# The third bit from the right represents 2**2 (which equals 4), when set
+def xor_basics():
+    a = 5
+    b = 3
 
-# first bit 2**0 = 1, bit set to 0 so,  0 * 1 = 0
-# second bit 2**1 = 2, bit set to 1 so,  1 * 2 = 2
-# third bit 2**2 = 4, bit set to 1 so,   1 * 4 = 4
-# 2 + 4 = 6
+    # Performing bitwise XOR
+    # numbers are converted to their binary representations
+    # 5 = 101 in binary
+    # 3 = 011 in binary
+    # Next the XOR operation is performed on each pair of bits (XOR yields 1 if bits are different, 0 if same)
+    result = a ^ b
+    # Starting from the RIGHT
+    # First bit   1 ^ 1 = 0
+    # Second bit  0 ^ 1 = 1
+    # Third bit   1 ^ 0 = 1
+    # resulting bits are compiled into binary 110, 6 in decimal
+    # The rightmost bit represents 2**0 (which equals 1), when the bit is set
+    # The second bit from the right represents 2**1 (which equals 2), when set
+    # The third bit from the right represents 2**2 (which equals 4), when set
 
-# Show the binary representation
-# [2:] used to skip the first two characters of the string, which are 0b
-# it makes sense to remove the 0b in this context to focus on the binary digits themselves
-# if you're storing or using the binary representation programmatically, you might keep the 0b
-# to maintain clarity that the string represents a binary number.
-binary_a = bin(a)[2:]
-binary_b = bin(b)[2:]
-binary_result = bin(result)[2:]
+    # first bit 2**0 = 1, bit set to 0 so,  0 * 1 = 0
+    # second bit 2**1 = 2, bit set to 1 so,  1 * 2 = 2
+    # third bit 2**2 = 4, bit set to 1 so,   1 * 4 = 4
+    # 2 + 4 = 6
 
-print(f' Binary of 5: {binary_a}')
-print(f' Binary of 3: {binary_b}')
-print(f' Binary result of 5 ^ 3: {binary_result}')
-print(f' Decimal result of 5 ^ 3: {result}')
+    # Show the binary representation
+    # [2:] used to skip the first two characters of the string, which are 0b
+    # it makes sense to remove the 0b in this context to focus on the binary digits themselves
+    # if you're storing or using the binary representation programmatically, you might keep the 0b
+    # to maintain clarity that the string represents a binary number.
+    binary_a = bin(a)[2:]
+    binary_b = bin(b)[2:]
+    binary_result = bin(result)[2:]
+
+    print(f' Binary of 5: {binary_a}')
+    print(f' Binary of 3: {binary_b}')
+    print(f' Binary result of 5 ^ 3: {binary_result}')
+    print(f' Decimal result of 5 ^ 3: {result}')
 
 
+def xor_data_masking(char, key):
+    """
+    Data is masked (or encrypted) by XORing it with a key
+    Same key is used to unmask(or decrypt) the data
+    """
+
+    # Let's consider a simple single character
+    # We encrypted 'A' using an XOR operation with the key '123'. The encrypted(masked) value is '58'
+    # When we decrypted(unmasked) this value using the same key, returned the orginal character 'A'
+
+    # Convert to ASCII
+    ascii_value = ord(char)
+
+    #XOR the ASCII value with the key
+    masked_value = ascii_value ^ key
+
+    # To decrypt, XOR the masked value with the same key
+    original_value = masked_value ^ key
+    decrypted_char = chr(original_value)
+
+    return masked_value, decrypted_char
+
+
+if __name__ == '__main__':
+    xor_basics()
+
+    char_to_encrypt = 'A'
+    encryption_key = 123 # Arbitrary key
+
+    masked, decrypted = xor_data_masking(char_to_encrypt, encryption_key)
+    print(f"Masked Value, {masked}")
+    print(f"Decrypted Character, {decrypted}")
