5a331c29822d629df2c7d472758c1c51278e840d
Mike Nardella <1310994+nardellajr@users.noreply.github.com>
2023-11-28 22:08:07 -0500

    added one possible solution to 1588

--- arrays/1588_sum_odd_length_subarrays.py
+++ arrays/1588_sum_odd_length_subarrays.py
@@ -4,15 +4,27 @@
 
 def sumOddlengthSubarrays(arr: list[int]) -> int:
     arr_dict = {}
-    for n in arr:
-        print(n)
-
+    for n in range(len(arr)):
+        if n % 2 == 0:
+            print(f'Even: {n}')
+        else:
+            print(f'Odd: {n}')
+            # based on Odd index, that is the number of elements we need to add
     return 99
 
+def sum_odd_length_subarray(arr: list[int]) -> int:
+    t = 0
+    l = len(arr)
+    for i in range(l):
+        for j in range(i, l, 2):  # step 2, to get odd values
+            t += sum(arr[i:j + 1])
+    return t
 
 if __name__ == '__main__':
     arr = [1, 4, 2, 5, 3]
-    sumOddlengthSubarrays(arr)
+    print(f'test: {sumOddlengthSubarrays(arr)}')
 
-    
+    print(f'working: {sum_odd_length_subarray(arr)}')
+
+
 
