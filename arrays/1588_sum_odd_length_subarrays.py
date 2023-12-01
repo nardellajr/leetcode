8889ae6d1f5d5fb8256be53c14227025e9453522
Mike Nardella <1310994+nardellajr@users.noreply.github.com>
2023-11-30 21:28:09 -0500

    add another solution to 1588 sum all odd length subarrays

--- arrays/1588_sum_odd_length_subarrays.py
+++ arrays/1588_sum_odd_length_subarrays.py
@@ -20,11 +20,38 @@ def sum_odd_length_subarray(arr: list[int]) -> int:
             t += sum(arr[i:j + 1])
     return t
 
+def sum_odd_length_subarray2(arr: list[int]) -> int:
+    # find the relationship between freq_current_element and freq_previous_element
+
+    res = 0
+    freq = 0
+    n = len(arr)
+    for i in range(n):
+        freq = freq - (i + 1)//2+(n-i+1)//2
+        res += freq * arr[i]
+
+    return res
+
+
 if __name__ == '__main__':
     arr = [1, 4, 2, 5, 3]
     print(f'test: {sumOddlengthSubarrays(arr)}')
 
     print(f'working: {sum_odd_length_subarray(arr)}')
 
+    print(f'subarray2: {sum_odd_length_subarray2(arr)}')
 
+
+# Explanation for sum_odd_length_subarray2
+# for example arr = [1,4,2,5,3], element arr[0] = 1. The appearing freq of head element arr[0]
+# should be how many odd-length sub arrays it can generate.
+# The answer is (len(arr)+1)//2. Therefore, the freq of element arr[0] = 1 is (5+1)//2=3.
+
+# Now let's take element arr[1] = 4 for example, if we take element arr[0] = 1 out,
+# then arr[1] = 4 becomes the new head element, thus the freq of arr[1] = 4 in the
+# new subarray could be calculated as the same way of arr[0] = 1.
+# It seems that all we need to do is add the freq of previous element arr[0] up then we get the freq of arr[1].
+
+# No, we also need to minus the subarrays of previous element arr[0] = 1 when they
+# do not include arr[1]=4. In this case, it is [1]. This is why freq[i] = freq[i-1]-(i+1)//2+(n-i+1)//2.
 
