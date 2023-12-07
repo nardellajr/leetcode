00970c5dc06e58e10e6515267e855bd0648e49df
Mike Nardella <1310994+nardellajr@users.noreply.github.com>
2023-12-06 22:49:17 -0500

    add print statements to follow the logic

--- arrays/1588_sum_odd_length_subarrays.py
+++ arrays/1588_sum_odd_length_subarrays.py
@@ -10,14 +10,18 @@ def sumOddlengthSubarrays(arr: list[int]) -> int:
         else:
             print(f'Odd: {n}')
             # based on Odd index, that is the number of elements we need to add
-    return 99
 
 def sum_odd_length_subarray(arr: list[int]) -> int:
+    print(f'sum old-length sub-arrays from: {arr}')
     t = 0
     l = len(arr)
     for i in range(l):
         for j in range(i, l, 2):  # step 2, to get odd values
-            t += sum(arr[i:j + 1])
+            sub_array = arr[i: j + 1]
+            print(sub_array)
+            k = sum(sub_array)
+            print(f'i: {i}, j + 1: { j + 1}, odd value total: {k}')
+            t+= k
     return t
 
 def sum_odd_length_subarray2(arr: list[int]) -> int:
@@ -35,7 +39,7 @@ def sum_odd_length_subarray2(arr: list[int]) -> int:
 
 if __name__ == '__main__':
     arr = [1, 4, 2, 5, 3]
-    print(f'test: {sumOddlengthSubarrays(arr)}')
+    # print(f'test: {sumOddlengthSubarrays(arr)}')
 
     print(f'working: {sum_odd_length_subarray(arr)}')
 
