ef8d2dbc995dd8821e3206059fc82d12ddd64663
Mike Nardella <1310994+nardellajr@users.noreply.github.com>
2023-12-11 21:44:44 -0500

    create shell for leet problem 2574

---
+++ arrays/2574_left_n_right_sum_diff.py
@@ -0,0 +1,15 @@
+# 2574 Left and Right sum difference
+# Given a 0-indexed integer array nums, find a 0-indexed array answer where:
+# answer.length == nums.length
+# answer[i] = [leftSum[i] - rightSum[i]]
+
+
+def left_right_difference(nums: list[int]) -> list[int]:
+    pass
+
+
+if __name__ == '__main__':
+    nums = [10, 4, 8, 3]
+    print(left_right_difference(nums))
+
+    
