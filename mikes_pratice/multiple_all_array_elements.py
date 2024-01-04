cd22afdeab58ea9bb48b91e1b289a3b0399aa82c
nardellajr <1310994+nardellajr@users.noreply.github.com>
2024-01-04 10:52:52 -0500

    add some comments

--- mikes_pratice/multiple_all_array_elements.py
+++ mikes_pratice/multiple_all_array_elements.py
@@ -1,5 +1,6 @@
 # Create a method that multiplies each element of the array
-
+# to each element of the array
+# [2,3,4]  = 2 * 2, 2 * 3, 2 * 4, 3 * 2, 3 * 3, 3 * 4, 4 * 2, 4 * 3, 4 * 4 = 81
 
 # Time complexity is O(n**2)
 def cal_each_element(arr):
@@ -23,9 +24,12 @@ def recursive_cal(arr, start=0, total=0):
     return recursive_cal(arr, start + 1, total)
 
 
+
+
 if __name__ == '__main__':
     arr = [2, 3, 7, 8, 10]
     # arr = [2, 3]
     print(cal_each_element(arr))
 
+    arr = [2, 3, 4]
     print(recursive_cal(arr))
